a56fbeaf70d6c9df4fb679e4895a1a730c247451
dharmicksai <dharmicksaik@gmail.com>
2021-03-09 23:21:40 +0530

    Added Schema Diagram and Abstract

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var mysql = require('mysql2');
 
 var dbms = mysql.createConnection({
     host: "localhost",
-    user: "admin",
-    password: "admin", // Change password for you!!!
+    user: "root",
+    password: "root@I", // Change password for you!!!
     database: "Platform"
 });
 
